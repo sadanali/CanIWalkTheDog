efbc4fab5810bab5cc9d4ed06cfe8562094224be
Su Adanali <goksuadanali@gmail.com>
2020-10-20 19:16:55 -0400

    Updates

---
+++ precache-manifest.9a93cfd8547ae169461b8cbd7d4e29f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cd237dae2abd0ab55280bc13acbf3fd",
+    "url": "/https://github.com/sadanali/can-i-walk-the-dog/index.html"
+  },
+  {
+    "revision": "d334582c489e913f951f",
+    "url": "/https://github.com/sadanali/can-i-walk-the-dog/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fdf45e06527fbb232d53",
+    "url": "/https://github.com/sadanali/can-i-walk-the-dog/static/js/2.a562a7f5.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/https://github.com/sadanali/can-i-walk-the-dog/static/js/2.a562a7f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d334582c489e913f951f",
+    "url": "/https://github.com/sadanali/can-i-walk-the-dog/static/js/main.7128b1d8.chunk.js"
+  },
+  {
+    "revision": "80e15d16599c0be58a6e",
+    "url": "/https://github.com/sadanali/can-i-walk-the-dog/static/js/runtime-main.dc32d7de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/https://github.com/sadanali/can-i-walk-the-dog/static/media/logo.5d5d9eef.svg"
+  }
+]);
