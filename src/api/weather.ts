84bd3faffd1df302ca26a01da183b3316ec27f86
Su Adanali <goksuadanali@gmail.com>
2020-10-26 18:42:37 -0400

    Show weather for zip code when you do a Get Weather

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -3,7 +3,7 @@ import { WEATHER_API_KEY } from './../ApiKeys';
 export async function fetchWeatherFromApi( zipCode: string ) {
     const weatherURL = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&units=imperial&APPID=${WEATHER_API_KEY}`
 
-    let response = await fetch(weatherURL);
+    let response = await fetch( weatherURL );
 
     if ( response.ok ) {
         let data = await response.json();
--- src/components/WeatherContainer.tsx
+++ src/components/WeatherContainer.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
 import { fetchWeatherFromApi } from './../api/weather'
 
-class WeatherContainer extends React.Component {
-    async getWeather() {
+type State = {
+    weather: number;
+}
+
+type Props = {
+}
+
+class WeatherContainer extends React.PureComponent<Props, State> {
+    state: State = {
+        weather: 0
+    }
+
+    getWeather = async ()  => {
         let data = await fetchWeatherFromApi('48842');
+        this.setState( { weather: data.main.temp } );
         console.log( data );
     }
 
     render() 
     {
         return(
-            <button onClick={this.getWeather}>GetWeather</button>
+            <div>
+                <div>
+                    <span>Weather data</span>
+                    <span>{this.state.weather}</span>
+                </div>
+                <button onClick={this.getWeather}>GetWeather</button>
+            </div>
         )
     }
 }
